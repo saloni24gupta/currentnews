226ad2289f68e67ffdc110e2ca4b77c87a51769a
saloni24gupta <sweety24saloni@gmail.com>
2021-09-25 11:50:00 -0700

    last

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-
 import './App.css';
 import NavBar from './Components/NavBar';
 import News from './Components/News';
-
 import LoadingBar from 'react-top-loading-bar'
 
-// import {
-  // BrowserRouter as Router,
-  // Switch,
-  // Route,
-// } from "react-router-dom";
-
+import
+ {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 import React, { Component } from 'react'
+export default class App extends Component {
 
-export default class App extends Component
- {
   pageSize = 5;
-apiKey = process.env.REACT_APP_NEWS_API
-  state = {
-    progress:0
+  apiKey = process.env.REACT_APP_NEWS_API
+  state = 
+  {
+  progress:0
   }
 
-  setProgress = (progress)=>{
-    this.setState({progress: progress})
+  setProgress = (progress)=>
+  {
+  this.setState({progress: progress})
   }
-  render() {
+  render() 
+  {
     return (
-      <div>
+     <>
 
-        {/* <Router> */}
+        <Router>
           <NavBar />
           <LoadingBar
         height={3}
@@ -37,33 +37,24 @@ apiKey = process.env.REACT_APP_NEWS_API
         progress={this.state.progress} 
       />
 
-<News apiKey={this.apiKey} setProgress={this.setProgress} key="Sports" pageSize={5} country="in" category="Sports" />
-           <News apiKey={this.apiKey}  setProgress={this.setProgress}  key="entertainment" pageSize={5} country="in" category="entertainment" />
-          <News apiKey={this.apiKey} setProgress={this.setProgress}   key="Health" pageSize={5} country="in" category="Health" />
-            <News  apiKey={this.apiKey}setProgress={this.setProgress}  key="Technology" pageSize={5} country="in" category="Technology" />
-            <News apiKey={this.apiKey}  setProgress={this.setProgress}  key="General" pageSize={5} country="in" category="general" />
-           <News   apiKey={this.apiKey} setProgress={this.setProgress}  key="Science" pageSize={5} country="in" category="Science" />
-           <News apiKey={this.apiKey} setProgress={this.setProgress}  key="Business" pageSize={5} country="in" category="Business" />
-
-          {/* <Switch> */}
-            {/* <Route exact path="/Sports"><News apiKey={this.apiKey} setProgress={this.setProgress} key="Sports" pageSize={5} country="in" category="Sports" /></Route>
-            <Route exact path="/Entertainment"><News apiKey={this.apiKey}  setProgress={this.setProgress}  key="entertainment" pageSize={5} country="in" category="entertainment" /></Route>
+          <Switch>
+            <Route exact path="/Sports"><News apiKey={this.apiKey} setProgress={this.setProgress} key="Sports" pageSize={5} country="in" category="Sports" /></Route>
+            <Route exact path="/Entertainment"><News  apiKey={this.apiKey} setProgress={this.setProgress}  key="entertainment" pageSize={5} country="in" category="entertainment" /></Route>
             <Route exact path="/Health"><News apiKey={this.apiKey} setProgress={this.setProgress}   key="Health" pageSize={5} country="in" category="Health" /></Route>
             <Route exact path="/Technology"><News  apiKey={this.apiKey}setProgress={this.setProgress}  key="Technology" pageSize={5} country="in" category="Technology" /></Route>
             <Route exact path="/General"><News apiKey={this.apiKey}  setProgress={this.setProgress}  key="General" pageSize={5} country="in" category="general" /></Route>
             <Route exact path="/Science"><News   apiKey={this.apiKey} setProgress={this.setProgress}  key="Science" pageSize={5} country="in" category="Science" /></Route>
-            <Route exact path="/Business"><News apiKey={this.apiKey} setProgress={this.setProgress}  key="Business" pageSize={5} country="in" category="Business" /></Route> */}
-          {/* </Switch> */}
+            <Route exact path="/Business"><News apiKey={this.apiKey} setProgress={this.setProgress}  key="Business" pageSize={5} country="in" category="Business" /></Route>
+          </Switch>
 
 
-        {/* </Router>); */}
-
-
-
-      </div>
+        </Router>);
+      </>
     )
-  }
 
+    }
+    
 }
+
 
 // export default App;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,21 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const NavBar = (props) => {
         return (
             <div className="container" >
                 <div className=" navbar-expand-sm  " style={{color: '#bed5f7'}} >
                     <div className="container-fluid" style={{backgroundColor: '#2b3848', color: '#bed5f7'}}>
-                        <a className="navbar-brand " href="/">hello</a>
+                        <Link className="navbar-brand " to="/"></Link>
                         <ul className="navbar-nav me-auto mb-2">
                
-            <li  className="nav-item"><a className ="nav-link " style={{color: '#bac5d4'}}  href="/Sports">Sports</a></li>
-                <li  className="nav-item"><a className ="nav-link" style={{color: '#bac5d4'}}  href="/entertainment" >entertainment</a></li>
-                <li  className="nav-item"><a className ="nav-link" style={{color: '#bac5d4'}}  href="/Health">Health</a></li>
-                <li  className="nav-item"><a className ="nav-link" style={{color: '#bac5d4'}}  href="/Technology" >Technology</a></li>
-                <li  className="nav-item"><a className ="nav-link" style={{color: '#bac5d4'}}  href="/General" >General</a></li>
-                <li  className="nav-item"><a className ="nav-link" style={{color: '#bac5d4'}} href="/Science" >Science</a></li>
-                <li  className="nav-item"><a className ="nav-link" style={{color: '#bac5d4'}} href="/Business" >Business</a></li>
+            <li  className="nav-item"><Link className ="nav-link " style={{color: '#bac5d4'}}  to="/Sports">Sports</Link></li>
+                <li  className="nav-item"><Link className ="nav-link" style={{color: '#bac5d4'}}  to="/entertainment" >entertainment</Link></li>
+                <li  className="nav-item"><Link className ="nav-link" style={{color: '#bac5d4'}}  to="/Health">Health</Link></li>
+                <li  className="nav-item"><Link className ="nav-link" style={{color: '#bac5d4'}}  to="/Technology" >Technology</Link></li>
+                <li  className="nav-item"><Link className ="nav-link" style={{color: '#bac5d4'}}  to="/General" >General</Link></li>
+                <li  className="nav-item"><Link className ="nav-link" style={{color: '#bac5d4'}} to="/Science" >Science</Link></li>
+                <li  className="nav-item"><Link className ="nav-link" style={{color: '#bac5d4'}} to="/Business" >Business</Link></li>
                <div className="container">
              
                </div>
