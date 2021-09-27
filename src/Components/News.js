839ca548bf274b50b2f436b326e5294989a85063
saloni24gupta <sweety24saloni@gmail.com>
2021-09-26 22:44:47 -0700

    first

--- src/Components/News.js
+++ src/Components/News.js
@@ -65,8 +65,10 @@ export class News extends Component {
     fetchMoreData = async () => {  
         this.setState({page: this.state.page + 1})
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        
         let data = await fetch(url);
         let parsedData = await data.json()
+        console.log(parsedData);
         this.setState({
             articles: this.state.articles.concat(parsedData.articles),
             totalResults: parsedData.totalResults
